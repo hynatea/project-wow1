6ba825e21ac5a5a54ed1eda0be4e57b7819fd3b6
melissa <boyrazmlss@gmail.com>
2024-12-12 17:48:41 +0100

    update services+login+homepage
    Création d'une page "Services" séparément de la Homepage.
    Modification de la Loginpage (CSS).
    Modification de la Homepage (CSS) et ajout image + quelques modif HTML

--- js/homepage.js
+++ js/homepage.js
@@ -4,7 +4,7 @@ let navLinks = document.querySelectorAll("header nav a");
 window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
+    let offset = sec.offsetTop;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
